c277ab39d47ac65d92013d935942c75412ba32cb
AkmalMaulana55 <vylingsmall55@gmail.com>
2025-05-08 19:53:15 +0700

    menambahkan delnode

--- singly.cpp
+++ singly.cpp
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 using namespace std;
 
@@ -78,4 +76,20 @@ public:
         
         return;
     }
+
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+        if (!Search(nim, &previous, &current))
+            return false;
+
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+            
+        delete current;
+        return true;
+    }
+    
 }
