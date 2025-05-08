97e496f712fee9bb040b876e1c267803b0de0dbf
AkmalMaulana55 <vylingsmall55@gmail.com>
2025-05-08 18:38:15 +0700

    menambahkan perintah memasukkan nim

--- singly.cpp
+++ singly.cpp
@@ -17,4 +17,15 @@ public:
     {
         START = NULL;
     }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa: ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+        
+    }
 }
